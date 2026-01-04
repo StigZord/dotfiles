49189da26de64fd3590983af84f55afcf1349386
m4teusz <m4teusz.zalewski@gmail.com>
2026-01-04 01:15:49 +0100

    feat: nvim snacks changes and plugin updates

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -4,23 +4,97 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = { enabled = true },
-    explorer = { enabled = true, hidden = true },
+    dashboard = {
+      sections = {
+        { section = 'terminal', cmd = 'toilet -f smmono12 (basename (pwd))', hl = 'header', padding = 1, indent = 8 },
+        {
+          pane = 2,
+          section = 'terminal',
+          cmd = 'colorscript -e square',
+          height = 5,
+          padding = 1,
+        },
+        { section = 'keys', gap = 1, padding = 1 },
+        {
+          pane = 2,
+          icon = ' ',
+          desc = 'Browse Repo',
+          padding = 1,
+          key = 'b',
+          action = function()
+            Snacks.gitbrowse()
+          end,
+        },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = 'Notifications',
+              cmd = 'gh notify -s -a -n5',
+              action = function()
+                vim.ui.open 'https://github.com/notifications'
+              end,
+              key = 'n',
+              icon = ' ',
+              height = 5,
+              enabled = true,
+            },
+            {
+              title = 'Open Issues',
+              cmd = 'gh issue list -L 3',
+              key = 'i',
+              action = function()
+                vim.fn.jobstart('gh issue list --web', { detach = true })
+              end,
+              icon = ' ',
+              height = 7,
+            },
+            {
+              icon = ' ',
+              title = 'Open PRs',
+              cmd = 'gh pr list -L 3',
+              key = 'P',
+              action = function()
+                vim.fn.jobstart('gh pr list --web', { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = ' ',
+              title = 'Git Status',
+              cmd = 'git --no-pager diff --stat -B -M -C',
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend('force', {
+              pane = 2,
+              section = 'terminal',
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
+        { section = 'startup' },
+      },
+    },
+    gh = {},
+    explorer = {},
     indent = {
       enabled = true,
       animate = { enabled = false },
     },
     input = { enabled = true },
+    lazygit = {},
     picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    -- scroll = { enabled = true },
     statuscolumn = { enabled = true },
+    toggle = { enabled = true },
     words = { enabled = true },
     ---
     styles = {
@@ -78,6 +152,11 @@ return {
     { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
     { "<leader>gh", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
     { "<leader>gu", function() Snacks.picker.git_status() end, desc = "Git Status" },
+    -- gh
+    { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+    { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+    { "<leader>gPo", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+    { "<leader>gPa", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
 
     -- Grep
     { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
@@ -123,6 +202,7 @@ return {
     { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
     { "<leader>uz",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>uZ",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
+    {"<leader>uh", function() Snacks.toggle.inlay_hints() end, desc = "Toggle inlay_hints"},
 
     -- Other
     { "<leader>tt",  function() Snacks.toggle() end, desc = "Snacks Toggler" },
@@ -151,4 +231,16 @@ return {
       end,
     }
   },
+  init = function()
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'VeryLazy',
+      callback = function()
+        Snacks.toggle.option('spell', { name = 'Spelling' }):map '<leader>ts'
+        Snacks.toggle.diagnostics():map '<leader>td'
+        Snacks.toggle.inlay_hints():map '<leader>th'
+        Snacks.toggle.indent():map '<leader>tg'
+        Snacks.toggle.dim():map '<leader>tD'
+      end,
+    })
+  end,
 }
